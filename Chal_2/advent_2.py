51be149ef11908a8f4596382ac1d9432ed2b3c9c
atg-abhijay <atg.abhijay@gmail.com>
2018-01-02 17:55:12 -0500

    solved part 1

--- Chal_2/advent_2.py
+++ Chal_2/advent_2.py
@@ -1,21 +1,34 @@
-def main():
-    """
-    URL for challenge: http://adventofcode.com/2017/day/2
-    """
+"""
+URL for challenge: http://adventofcode.com/2017/day/2
+"""
+
+def part1():
     f = open("advent_2_input.txt")
-    data = f.read()
-    return data
+    checksum = 0
+    for line in f.readlines():
+        # remove the ending '\n'
+        line = line.rstrip()
+        # split the line into a list
+        # using tabs ('\t') as delimiters
+        string_list = line.split('\t')
+        # the above list obtained has the
+        # numbers stored as strings. use map()
+        # to map each of the elements of string_list
+        # to an int. cast the map object to a list
+        int_list = list(map(int, string_list))
+        checksum += max(int_list) - min(int_list)
+
+    print(checksum)
 
 def run():
     chall = int(input("Please enter either 1 or 2 for the challenges: "))
-    data = main()
     if chall == 1:
-        part1(data)
+        part1()
     elif chall == 2:
-        part2(data)
+        part2()
     else:
         print("You need to enter either 1 or 2")
         exit(1)
 
-main()
+part1()
 # run()
