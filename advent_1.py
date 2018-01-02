093fda7e3b480f2cc2fc2ff295eda243eddf17bc
atg-abhijay <atg.abhijay@gmail.com>
2018-01-02 17:13:41 -0500

    adding a run structure for the challenges

--- advent_1.py
+++ advent_1.py
@@ -1,9 +1,14 @@
+from sys import exit
+
 def main():
     """
     URL for challenge: http://adventofcode.com/2017/day/1
     """
     f = open("advent_1_input.txt")
     data = f.read()
+    return data
+
+def part1(data):
     sum_of_numbers = 0
     for i in range(len(data)-2):
         digit = int(data[i])
@@ -16,4 +21,20 @@ def main():
 
     print(sum_of_numbers)
 
+def part2(data):
+    sum_of_numbers = 0
+    print()
+
+def run():
+    chall = int(input("Please enter either 1 or 2 for either challenge"))
+    data = main()
+    if chall == 1:
+        part1(data)
+    elif chall == 2:
+        part2(data)
+    else:
+        print("You need to enter either 1 or 2")
+        exit(1)
+
 main()
+# run()
