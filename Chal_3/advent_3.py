9353893e3909331dfd62dad55ca59e05de927c9c
atg-abhijay <atg.abhijay@gmail.com>
2018-01-02 23:33:50 -0500

    solved part 2. not a really elegant solution

--- Chal_3/advent_3.py
+++ Chal_3/advent_3.py
@@ -4,6 +4,9 @@ import math
 URL for challenge: http://adventofcode.com/2017/day/3
 """
 
+puzzle_input = 265149
+grid = dict()
+
 def part1():
     """
     The Manhattan Distance would be:
@@ -17,7 +20,6 @@ def part1():
 
     # let counting of layers start from 1
     # i.e. the number '1' is in layer 1
-    puzzle_input = 265149
     temp = (math.sqrt(puzzle_input) + 1)/2
     # puzzle input lies in this layer
     layer = math.ceil(temp)
@@ -55,20 +57,113 @@ def part1():
 
 
 def part2():
+    """
+    (0,0) is initialized to '1'.
+    check all the neighbours of the
+    point under consideration.
 
+    couldn't really think of a
+    better method. solution comes fast
+    though.
+    """
+    grid[(0, 0)] = 1
+    coordinates_value = 0
+    layer = 1
+    x = 0; y = 0
+    done = False
+    while not done:
+        # print("Layer: ", layer)
+        # go right one step
+        layer += 1; x += 1
+        grid[(x,y)] = check_neighbours((x,y))
+
+        # go up to the boundary of layer
+        for y_up in range(y+1, layer):
+            coord = (x, y_up)
+            coordinates_value = check_neighbours(coord)
+            if coordinates_value > puzzle_input:
+                return coordinates_value
+        y = y_up
+
+        # go left till the boundary of layer
+        for x_left in range(x-1, -layer, -1):
+            coord = (x_left, y)
+            coordinates_value = check_neighbours(coord)
+            if coordinates_value > puzzle_input:
+                return coordinates_value
+        x = x_left
+
+        # go down till the boundary of layer
+        for y_down in range(y-1, -layer, -1):
+            coord = (x, y_down)
+            coordinates_value = check_neighbours(coord)
+            if coordinates_value > puzzle_input:
+                return coordinates_value
+        y = y_down
+
+        # go right till the boundary of layer
+        for x_right in range(x+1, layer):
+            coord = (x_right, y)
+            coordinates_value = check_neighbours(coord)
+            if coordinates_value > puzzle_input:
+                return coordinates_value
+        x = x_right
+
+
+def check_neighbours(coordinates):
+    """
+    checking existence of all neighbours
+    of the point given by coordinates
+
+    actually checking itself as well.
+    but since it hasn't been initialized
+    yet, it's not a problem
+    """
+    x_coord = coordinates[0]
+    y_coord = coordinates[1]
+    coordinates_value = 0
+    for x_move in [-1, 0, 1]:
+        x = x_coord + x_move
+        for y_move in [-1, 0, 1]:
+            y = y_coord + y_move
+            try:
+                value = grid[(x,y)]
+                coordinates_value += value
+            except KeyError:
+                pass
+
+    grid[coordinates] = coordinates_value
+    # print(coordinates_value)
+    return coordinates_value
 
 def run():
-    chall = int(input("Please enter either 1 or 2 for the challenges: "))
+    # chall = int(input("Please enter either 1 or 2 for the challenges: "))
+    chall = 2
     if chall == 1:
         part1()
     elif chall == 2:
-        part2()
+        result = part2()
+        print(result)
     else:
         print("You need to enter either 1 or 2")
         exit(1)
 
 def test():
-    for i in range(4):
+    for i in range(-1,4):
         print(i)
 
+    x = 5
+    print(-x)
+
+    # coord = (0, 3)
+    # d = dict()
+    # d[coord] = 4
+    # print(d[(0, 3)])
+    # try:
+    #     print(d[(0, 2)])
+    # except KeyError:
+    #     print("sorry")
+
+
 run()
+# test()
