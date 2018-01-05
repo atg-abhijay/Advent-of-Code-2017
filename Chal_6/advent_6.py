b150d47c61f73a358578cd1ff098885866238628
atg-abhijay <atg.abhijay@gmail.com>
2018-01-05 00:09:42 -0500

    good question; solved part 2; added more comments; copied code from part1 since didn't want to make a messy general method. code used is now clear for both parts, even though it's repeated

--- Chal_6/advent_6.py
+++ Chal_6/advent_6.py
@@ -12,7 +12,10 @@ def part1(banks):
     # dictionary which keeps track
     # of configurations observed
     configs_observed = {}
-    # adding the first configuration
+    # adding the first configuration.
+    # have to use a tuple since a
+    # dictionary cannot have a
+    # mutable object as a key
     first_banks_tuple = tuple(banks)
     configs_observed[first_banks_tuple] = True
 
@@ -24,9 +27,9 @@ def part1(banks):
         blocks_to_move = max(banks)
         # using index() also takes care of the
         # fact that in case of a tie for max
-        # value, the lower index should be picked.
-        # since index() gives the first occurence
-        # of that max value
+        # value, the lower index should be picked
+        # (since index() gives the first occurence
+        # of that max value)
         max_index = banks.index(blocks_to_move)
         # emptying the bank at that index
         banks[max_index] = 0
@@ -37,17 +40,54 @@ def part1(banks):
             blocks_to_move -= 1
             banks[pointer] += 1
 
-        # have to use a tuple since a
-        # dictionary cannot have a
-        # mutable object as a key
         banks_tuple = tuple(banks)
         if banks_tuple not in configs_observed:
             configs_observed[banks_tuple] = True
         else:
             repeat_obtained = True
+            # printing the repeated config
             print(banks)
 
     print(num_cycles)
+
+
+def part2(banks):
+    # banks = [1, 2, 1, 0]
+    num_banks = len(banks)
+    num_cycles = 0
+    configs_observed = {}
+    first_banks_tuple = tuple(banks)
+    # the value is the cycle number
+    # at which we are currently
+    configs_observed[first_banks_tuple] = num_cycles
+
+    repeat_obtained = False
+    num_cycles_loop = 0
+    while not repeat_obtained:
+        num_cycles += 1
+        blocks_to_move = max(banks)
+        max_index = banks.index(blocks_to_move)
+        banks[max_index] = 0
+
+        pointer = max_index
+        while blocks_to_move > 0:
+            pointer = (pointer + 1) % num_banks
+            blocks_to_move -= 1
+            banks[pointer] += 1
+
+        banks_tuple = tuple(banks)
+        if banks_tuple not in configs_observed:
+            configs_observed[banks_tuple] = num_cycles
+        else:
+            repeat_obtained = True
+            print(banks)
+            # number of cycles in the loop will be
+            # the current number of cycles minus the
+            # number of cycles when this config
+            # occured previously
+            num_cycles_loop = num_cycles - configs_observed[banks_tuple]
+
+    print(num_cycles_loop)
 
 def run():
     chall = int(input("Please enter either 1 or 2 for the challenges: "))
