d0790870fa75014522cc33cdab3fb83674e95b7d
atg-abhijay <atg.abhijay@gmail.com>
2018-01-04 23:36:07 -0500

    solved part 1. pretty interesting question

--- Chal_6/advent_6.py
+++ Chal_6/advent_6.py
@@ -1,11 +1,48 @@
 def main():
     f = open("advent_6_input.txt")
     input_line = f.readline()
-    banks = input_line.split('\t')
+    banks_strings = input_line.split('\t')
+    banks = list(map(int, banks_strings))
+    # print(type(banks))
     return banks
 
 def part1(banks):
-    pass
+    # banks = [0, 2, 7, 0]
+    num_banks = len(banks)
+    num_cycles = 0
+    # dictionary which keeps track
+    # of configurations observed
+    configs_observed = {}
+    repeat_obtained = False
+    while not repeat_obtained:
+        num_cycles += 1
+        # finding the maximum number of blocks
+        # i.e. number of blocks to move
+        blocks_to_move = max(banks)
+        # using index() also takes care of the
+        # fact that in case of a tie for max
+        # value, the lower index should be picked.
+        # since index() gives the first occurence
+        # of that max value
+        max_index = banks.index(blocks_to_move)
+        # emptying the bank at that index
+        banks[max_index] = 0
+
+        pointer = max_index
+        while blocks_to_move > 0:
+            pointer = (pointer + 1) % num_banks
+            blocks_to_move -= 1
+            banks[pointer] += 1
+
+        banks_tuple = tuple(banks)
+        if banks_tuple not in configs_observed:
+            configs_observed[banks_tuple] = True
+        else:
+            repeat_obtained = True
+
+        # print(banks)
+
+    print(num_cycles)
 
 def run():
     chall = int(input("Please enter either 1 or 2 for the challenges: "))
@@ -18,5 +55,19 @@ def run():
         print("You need to enter either 1 or 2")
         exit(1)
 
-main()
-# run()
+def test():
+    l1 = [1, 2, 3, 4, 25, 5, 7, 9, 25]
+    l2 = [1, 2, 3, 4, 25, 6, 5, 7, 9, 25]
+    t1 = (4, 5, 2, 245, 2134)
+    print(len(t1))
+    print(t1[4])
+    # d = {l1: False, l2: True}
+    # print(d[tuple(l2)])
+    # print(l1 >= l2)
+    # m = max(l)
+    # index = l.index(m)
+    # print(m, index)
+
+# test()
+# main()
+run()
