03ab91a02b047309c8d5b742379c09cd678cc85d
atg-abhijay <atg.abhijay@gmail.com>
2018-01-17 18:21:25 -0500

    initial commit

---
+++ Chal_10/advent_10.py
@@ -0,0 +1,11 @@
+def run():
+    chall = int(input("Please enter either 1 or 2 for the challenges: "))
+    if chall == 1:
+        part1()
+    elif chall == 2:
+        part2()
+    else:
+        print("You need to enter either 1 or 2")
+        exit(1)
+
+run()
