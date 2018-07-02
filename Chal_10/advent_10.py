b39bf42f7c6c2a840030269b548a2495359c38c4
atg-abhijay <atg.abhijay@gmail.com>
2018-07-02 12:12:14 -0400

    added comments for part 1

--- Chal_10/advent_10.py
+++ Chal_10/advent_10.py
@@ -7,6 +7,8 @@ def main():
     input_data = f.readline()
     # print(input_data)
     lengths = input_data.split(',')
+    # the lengths are currently stored as strings
+    # we map them to ints and convert the map to a list
     lengths = list(map(int, lengths))
     # print(lengths)
     return lengths
@@ -22,16 +24,20 @@ def part1(lengths):
         list_slice = []
         reverse_slice = []
         indices_changed = []
+        # extracting elements based on given length
         for i in range(length):
             index = (current_pos + i) % len_circ
             indices_changed.append(index)
+            # list_slice contains the elements to be reversed
             list_slice.append(circ_list[index])
 
+        # forming the reversed list
         for element in reversed(list_slice):
             reverse_slice.append(element)
 
         for i in range(len(indices_changed)):
             index = indices_changed[i]
+            # updating the circ_list with the reversed element
             circ_list[index] = reverse_slice[i]
 
         current_pos += length + skip_size
@@ -42,6 +48,9 @@ def part1(lengths):
     # print(circ_list)
     print(result)
 
+
+def part2():
+    pass
 
 def run():
     chall = int(input("Please enter either 1 or 2 for the challenges: "))
@@ -56,9 +65,9 @@ def run():
 
 
 def test():
-    l = [x for x in range(256)]
+    # l = [x for x in range(256)]
     # print(l)
-    p = [4,6,2,7,1,8]
+    # p = [4,6,2,7,1,8]
     # for elem in reversed(p):
     #     print(elem)
     # for i in range(1,6):
