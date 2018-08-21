2a5cb108a1d30d1a2186961f812d930109f93c8e
atg-abhijay <atg.abhijay@gmail.com>
2018-08-21 15:04:32 -0400

    q11: initial commit

---
+++ Chal_11/advent_11.py
@@ -0,0 +1,22 @@
+"""
+URL for challenge: https://adventofcode.com/2017/day/11
+"""
+
+def part1():
+    pass
+
+
+def part2():
+    pass
+
+
+def run():
+    chall = int(input("Please enter either 1 or 2 for the challenges: "))
+    if chall == 1:
+        part1()
+    elif chall == 2:
+        part2()
+    else:
+        print("You need to enter either 1 or 2")
+        exit(1)
+
