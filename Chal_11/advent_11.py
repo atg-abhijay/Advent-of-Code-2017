f5580e5aef76ddf6bfb57d8343eb136aaede4875
atg-abhijay <atg.abhijay@gmail.com>
2018-08-21 17:11:53 -0400

    q11: solved part 1 and added comments for it

--- Chal_11/advent_11.py
+++ Chal_11/advent_11.py
@@ -6,52 +6,62 @@ def main():
     f = open("advent_11_input.txt")
     data = f.readline()
     commands = data.split(',')
-    commands_dict = {
-        'n': (0, 2),
-        'ne': (1, 1),
-        'se': (1, -1),
-        's': (0, -2),
-        'sw': (-1, -1),
-        'nw': (-1, 1)
-    }
-    num_times = {'n': 0, 'ne': 0, 'se': 0, 's': 0, 'sw': 0, 'nw': 0}
+    # north is opposite to south
+    # north-east is oppposite to south-west
+    # north-west is oppposite to south-east
+    # therefore only one variable needed for
+    # each of the pairs.
+    num_times = {'n': 0, 'ne': 0, 'nw': 0}
     for command in commands:
         if command == 'n':
             num_times['n'] += 1
         elif command == 'ne':
             num_times['ne'] += 1
-        elif command == 'se':
-            num_times['se'] += 1
-        elif command == 's':
-            num_times['s'] += 1
-        elif command == 'sw':
-            num_times['sw'] += 1
         elif command == 'nw':
             num_times['nw'] += 1
-    print(num_times)
+        elif command == 's':
+            num_times['n'] -= 1
+        elif command == 'sw':
+            num_times['ne'] -= 1
+        elif command == 'se':
+            num_times['nw'] -= 1
+
+    return num_times
 
 
 def part1(num_times):
-    num_times['n'] -= num_times['s']
-    num_times['ne'] -= num_times['sw']
-    num_times['se'] -= num_times['nw']
+    # using variables for ease of writing code
+    n = num_times['n']
+    ne = num_times['ne']
+    nw = num_times['nw']
+    # if ne and nw are both positive
+    # or both negative, then their values
+    # combine to contribute to north
 
-    if abs(num_times['n']) > abs(num_times['se']):
-        num_times['ne'] += num_times['se']
-        num_times['n'] -= num_times['se']
-        num_times['se'] = 0
-        if num_times['n'] < 0:
-            num_times['s'] = abs(num_times['n'])
-            num_times['n'] = 0
-        if num_times['ne'] < 0:
-            num_times['sw'] = abs(num_times['ne'])
-            num_times['ne'] = 0
+    # if one of them is positive and the other
+    # is negative, then we can leave the values
+    # as is because there exists three unique
+    # directions of movement in the grid
+    if ne*nw > 0:
+        if abs(ne) < abs(nw):
+            n += ne
+            nw -= ne
+            ne = 0
+        else:
+            n += nw
+            ne -= nw
+            nw = 0
 
-    else:
-        num_times['ne'] += num_times['n']
-        num_times['se'] -= num_times['n']
-        num_times['n'] = 0
+        num_times['n'] = n
+        num_times['ne'] = ne
+        num_times['nw'] = nw
 
+    # irrespective of being positive or negative,
+    # the sum of the absolute values of the steps
+    # in the three directions is the total no. of steps
+    total_steps = abs(n)+abs(ne)+abs(nw)
+    print(num_times)
+    print("Total steps required:", total_steps)
 
 def part2():
     pass
@@ -59,8 +69,9 @@ def part2():
 
 def run():
     chall = int(input("Please enter either 1 or 2 for the challenges: "))
+    num_times = main()
     if chall == 1:
-        part1()
+        part1(num_times)
     elif chall == 2:
         part2()
     else:
@@ -74,5 +85,6 @@ def test():
 
     print(min(5,-4,2))
 
-test()
+# test()
 # main()
+run()
