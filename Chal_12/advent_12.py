a3d2be1b1bf99f6c5cef0f00abf22e4ead042183
atg-abhijay <atg.abhijay@gmail.com>
2018-08-22 19:30:01 -0400

    q12: able to solve part 1 using tinydb. will see if I can use a dict instead since it seems to be taking a fair amount of time to build the db

--- Chal_12/advent_12.py
+++ Chal_12/advent_12.py
@@ -1,56 +1,52 @@
-import random
 """
 URL for challenge: https://adventofcode.com/2017/day/12
 """
 
+from tinydb import TinyDB, Query
+
+db = TinyDB('db.json')
+
 def main():
     f = open("/Users/AbhijayGupta/Projects/Advent_of_Code_2017/Chal_12/advent_12_input.txt")
-    prog_conn = {}
+    prog = {'prog': 0, 'conns': set(), 'visited': False}
     for l in f.readlines():
         line = l.replace(' ', '').strip().split('<->')
-        prog = int(line[0])
+        prog_no = int(line[0])
         connections = line[1].split(',')
         # the numbers are stored as strings in the list
         # so we map them to integers and convert the
         # map object to a list
         conns = list(map(int, connections))
-        prog_conn[prog] = conns
+        prog['prog'] = prog_no
+        prog['conns'] = conns
+        db.insert(prog)
 
-    return prog_conn
 
-# adding a Node class to make a binary tree to
-# make it easier to check for program ids inserted
-class Node:
-    def __init__(self, data):
-        self.left = None
-        self.right = None
-        self.data = data
+def part1():
+    group_progid_0 = {0}
+    query_obj = Query()
+    prog0 = db.get(query_obj.prog == 0)
+    group_progid_0.update(prog0['conns'])
+    db.update({'visited': True}, query_obj.prog == 0)
+    group_progid_0 = find_progs(db.all(), group_progid_0)
+    print(group_progid_0)
+    print(len(group_progid_0))
 
-    def insert(self, data):
-        if self.data is not None:
-            if data < self.data:
-                if self.left is None:
-                    self.left = Node(data)
-                else:
-                    self.left.insert(data)
-            elif data > self.data:
-                if self.right is None:
-                    self.right = Node(data)
-                else:
-                    self.right.insert(data)
-        else:
-            self.data = data
+def find_progs(progs, group_progid_0):
+    query_obj = Query()
+    for prog in progs:
+        if not prog['visited']:
+            do_intersect = bool(group_progid_0.intersection(prog['conns']))
+            if do_intersect:
+                group_progid_0.add(prog['prog'])
+                db.update({'visited': True}, query_obj.prog == prog['prog'])
+                for conn in prog['conns']:
+                    conn = db.get(query_obj.prog == conn)
+                    find_progs([conn], group_progid_0)
 
-    def printTree(self):
-        if self.left:
-            self.left.printTree()
+    return group_progid_0
 
-        print(self.data)
 
-        if self.right:
-            self.right.printTree()
-
-def part1(prog_conn):
     # group_progid_0 = {0}
     # group_progid_0.update(prog_conn[0])
     # for key in prog_conn.keys():
@@ -63,24 +59,25 @@ def part1(prog_conn):
     # print(group_progid_0)
     # print(len(group_progid_0))
 
-    root = Node(0)
-    for conn in prog_conn[0]:
-        root.insert(conn)
+    # group_progid_0 = set()
+    # root = Node(0)
+    # for conn in prog_conn[0]:
+    #     root.insert(conn)
 
-    root.printTree()
 
 def run():
     # chall = int(input("Please enter either 1 or 2 for the challenges: "))
     chall = 1
-    prog_conn = main()
     if chall == 1:
-        part1(prog_conn)
+        part1()
     elif chall == 2:
         part2()
     else:
         print("You need to enter either 1 or 2")
         exit(1)
 
+def purge_db():
+    db.purge()
 
 def test():
     # s = "454, 528, 621, 1023, 1199"
@@ -89,11 +86,13 @@ def test():
     b = {4, 7, 6}
     # print(bool(a.intersection(b)))
     c = list(a.union(b))
-    for elem in c:
-        c.append(random.randint(11,20))
-        print(elem)
+    # for elem in c:
+    #     c.append(random.randint(11,20))
+    #     print(elem)
 
+    print(db.all())
 
 # test()
-# main()
+purge_db()
+main()
 run()
