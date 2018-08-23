afd46f9b3cadd165ebb74bef36b82a922e1ed0d2
atg-abhijay <atg.abhijay@gmail.com>
2018-08-22 20:50:38 -0400

    q12: added comments for part 1; removed unused code and deleted db.json

--- Chal_12/advent_12.py
+++ Chal_12/advent_12.py
@@ -7,73 +7,63 @@ def main():
     programs = {}
     for l in f.readlines():
         line = l.replace(' ', '').strip().split('<->')
-        prog_no = int(line[0])
+        prog_id = int(line[0])
         connections = line[1].split(',')
         # the numbers are stored as strings in the list
         # so we map them to integers and convert the
         # map object to a list
-        details = {}
         conns = list(map(int, connections))
+        # making a separate dictionary containing
+        # the connections of a particular id and
+        # the visit status of that id
+        details = {}
         details["conns"] = conns
         details["visited"] = False
-        programs[prog_no] = details
+        # the program id will be the key
+        # with the dict details as the value
+        programs[prog_id] = details
 
     # print(programs)
     return programs
 
 
 def part1(programs):
+    # Adding program 0 and its connections
+    # to the group and then setting
+    # the visit status to True
     group_progid_0 = {0}
     prog0 = programs[0]
     group_progid_0.update(prog0['conns'])
     programs[0]['visited'] = True
     group_progid_0 = find_progs(programs.keys(), programs, group_progid_0)
-    print(group_progid_0)
-    print(len(group_progid_0))
+    # print(group_progid_0)
+    print("Programs linked to program ID 0:", len(group_progid_0))
 
 
 def find_progs(keys, progs, group_progid_0):
-    # for prog in progs.values():
-    #     if not prog['visited']:
-    #         do_intersect = bool(group_progid_0.intersection(prog['conns']))
-    #         if do_intersect:
-    #             group_progid_0.add(prog['prog'])
-    #             db.update({'visited': True}, query_obj.prog == prog['prog'])
-    #             for conn in prog['conns']:
-    #                 conn = db.get(query_obj.prog == conn)
-    #                 find_progs([conn], group_progid_0)
-
-    # return group_progid_0
-
+    # check the visit status of each program and
+    # if not visited, then proceed further.
+    # if the connections of that program intersect
+    # with the ids already in the group, then add
+    # the id of that program to the group (update visit status)
+    # and look at the connections themselves.
+    # looking at the connections is a recursive step.
+    # we save time by checking for visit status
+    # at the beginning.
     for key in keys:
         details = progs[key]
         if not details['visited']:
-            do_intersect = bool(group_progid_0.intersection(set(details['conns'])))
+            do_intersect = bool(group_progid_0.intersection(details['conns']))
             if do_intersect:
                 group_progid_0.add(key)
                 progs[key]['visited'] = True
                 for conn in details['conns']:
+                    # have to put 'conn' in a list since
+                    # the loop looks at 'keys' which is an
+                    # iterable instance. cannot give an int
                     find_progs([conn], progs, group_progid_0)
 
     return group_progid_0
-
-
-    # group_progid_0 = {0}
-    # group_progid_0.update(prog_conn[0])
-    # for key in prog_conn.keys():
-    #     key_conns = set(prog_conn[key])
-    #     do_intersect = bool(group_progid_0.intersection(key_conns))
-    #     if do_intersect:
-    #         group_progid_0.add(key)
-    #         group_progid_0.update(key_conns)
-
-    # print(group_progid_0)
-    # print(len(group_progid_0))
-
-    # group_progid_0 = set()
-    # root = Node(0)
-    # for conn in prog_conn[0]:
-    #     root.insert(conn)
 
 
 def run():
@@ -88,8 +78,6 @@ def run():
         print("You need to enter either 1 or 2")
         exit(1)
 
-def purge_db():
-    db.purge()
 
 def test():
     # s = "454, 528, 621, 1023, 1199"
@@ -102,9 +90,6 @@ def test():
     #     c.append(random.randint(11,20))
     #     print(elem)
 
-    print(db.all())
-
 # test()
-# purge_db()
 # main()
 run()
