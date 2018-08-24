e4270aacc9aec6b433d6bd7bb46aa97e8fa06ef8
atg-abhijay <atg.abhijay@gmail.com>
2018-08-24 17:32:34 -0400

    q13: part 1 - added main method to collect data

--- Chal_13/advent_13.py
+++ Chal_13/advent_13.py
@@ -2,6 +2,20 @@
 URL for challenge: https://adventofcode.com/2017/day/13
 """
 
+def main():
+    f = open("advent_13_input.txt")
+    depth_range = {}
+    for l in f.readlines():
+        line = l.replace(' ', '').strip()
+        line_parts = line.split(':')
+        depth = int(line_parts[0])
+        scn_range = int(line_parts[1])
+        depth_range[depth] = scn_range
+
+    # print(depth_range)
+    return depth_range
+
+
 def run():
     chall = int(input("Please enter either 1 or 2 for the challenges: "))
     if chall == 1:
@@ -12,4 +26,6 @@ def run():
         print("You need to enter either 1 or 2")
         exit(1)
 
-run()
+
+main()
+# run()
